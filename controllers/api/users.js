731bc270a8f2732a88e9a6cda1c3b8f71eea01f0
Mohamed Abdulle <m7assan95@gmail.com>
2023-02-28 17:23:34 -0500

    MERN-Stack Infrastructure

---
+++ controllers/api/users.js
@@ -0,0 +1,52 @@
+const User = require("../../models/user");
+const jwt = require("jsonwebtoken");
+
+module.exports = {
+  create,
+  login,
+  checkToken,
+};
+
+const bcrypt = require("bcrypt");
+
+async function create(req, res) {
+  try {
+    // Add the user to the database
+    const user = await User.create(req.body);
+
+    const token = createJWT(user);
+
+    res.json(token);
+  } catch (err) {
+    // Client will check for non-2xx status code
+    // 400 = Bad Request
+    res.status(400).json(err);
+  }
+}
+
+function createJWT(user) {
+  return jwt.sign(
+    // data payload
+    { user },
+    process.env.SECRET,
+    { expiresIn: "24h" }
+  );
+}
+
+async function login(req, res) {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) throw new Error();
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (!match) throw new Error();
+    res.json(createJWT(user));
+  } catch {
+    res.status(400).json("Bad Credentials");
+  }
+}
+
+function checkToken(req, res) {
+  // req.user will always be there for you when a token is sent
+  console.log("req.user", req.user);
+  res.json(req.exp);
+}
